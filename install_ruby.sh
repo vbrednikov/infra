c0f4ba1d8c5ae21e503d7f68dec9c413570c747f
Vladimir Brednikov <vbrednikov@gmail.com>
2017-09-04 09:50:33 +0300

    echo "Install ruby and rvm" before installation

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo '--------- Install ruby and rvm ------'
 apt-get -y update && apt-get upgrade -y
 # install ruby for appuser
 sudo -u "appuser" bash -c 'gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && curl -sSL https://get.rvm.io | bash -s stable && . ~/.rvm/scripts/rvm && rvm requirements && rvm install 2.4.1 && rvm use 2.4.1 --default && gem install bundler -V --no-ri --no-rdoc' 
