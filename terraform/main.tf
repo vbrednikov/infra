39944e0500a433e3318e84209210907c5b97600f
Vladimir Brednikov <vbrednikov@gmail.com>
2017-09-12 00:20:16 +0300

    add default ssh rule

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,3 +53,16 @@ resource "google_compute_firewall" "firewall_puma" {
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["reddit-app"]
 }
+
+resource "google_compute_firewall" "firewall_ssh" {
+  name        = "default-allow-ssh"
+  network     = "default"
+  description = "Allow SSH from outside"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
