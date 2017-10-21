05bf8da829e5f52a050c79cf1b3e7f8679b39714
Vladimir Brednikov <vbrednikov@gmail.com>
2017-10-22 01:08:56 +0300

    add test for mongodb port 27017

--- ansible/roles/db/molecule/default/tests/test_default.py
+++ ansible/roles/db/molecule/default/tests/test_default.py
@@ -14,3 +14,6 @@ def test_config_file(host):
     config_file = host.file("/etc/mongod.conf")
     assert config_file.contains("bindIp: 0.0.0.0")
     assert config_file.is_file
+
+def test_mongo_listen_port(host):
+	assert host.socket("tcp://0.0.0.0:27017").is_listening
