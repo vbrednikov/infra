eeaf56a51ec0f7cdefd622062f94676bc42b0a2e
Vladimir Brednikov <vbrednikov@gmail.com>
2017-10-22 01:08:04 +0300

    Fix deprecation warning about File fixture

--- ansible/roles/db/molecule/default/tests/test_default.py
+++ ansible/roles/db/molecule/default/tests/test_default.py
@@ -10,7 +10,7 @@ def test_mongo_running_and_enabled(host):
     assert mongo.is_running
     assert mongo.is_enabled
 
-def test_config_file(File):
-    config_file = File("/etc/mongod.conf")
+def test_config_file(host):
+    config_file = host.file("/etc/mongod.conf")
     assert config_file.contains("bindIp: 0.0.0.0")
     assert config_file.is_file
